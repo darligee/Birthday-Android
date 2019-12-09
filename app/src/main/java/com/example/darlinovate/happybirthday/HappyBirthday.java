a273000a8980895eff8cc65a5f2998479d5d6782
darligee <darli.gee@gmail.com>
2019-12-09 14:24:59 +0100

    Birthday Card built using Java, XML and Android

---
+++ app/src/main/java/com/example/darlinovate/happybirthday/HappyBirthday.java
@@ -0,0 +1,13 @@
+package com.example.darlinovate.happybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HappyBirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_birthday);
+    }
+}
